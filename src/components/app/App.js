47078170aed8a08f47066c054b2521bc69eda1d5
lpoulter1 <thundercatontheloose@gmail.com>
2023-06-04 22:27:55 +0100

    simpler

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import "./App.css";
-import CardList from "../cardlist/cardlist";
 import Dropdown from "../dropdown/dropdown";
 import { teams, gameweeks } from "../dummy arrays/dummy";
+import "./App.css";
 
-// rendering the react elements
 export default function App() {
-  const [numberOfGameweeks, setNumberOfGameweeks] = useState(1);
+  const [numberOfGameweeks, setNumberOfGameweeks] = useState(3);
 
   const handleGameweekChange = (event) => {
     setNumberOfGameweeks(event.target.value);
@@ -32,9 +30,66 @@ export default function App() {
           <Dropdown handleGameweekChange={handleGameweekChange} />
         </div>
       </div>
-      <div className="cards">
-        <CardList teams={teams} gameweeks={filteredGameweeks} />
+      <div>
+        <FixtureTable teams={teams} gameweekFixtures={filteredGameweeks} />
       </div>
     </div>
+  );
+}
+
+function FixtureTable({ teams, gameweekFixtures }) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th> {/* Leave the first cell in the header row empty */}
+            {gameweekFixtures.map((_, index) => (
+              <th>{`GW ${index + 1}`}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team, teamIndex) => {
+            // gamweekfixtures are orderd by team,
+            // so we can use the teamIndex to get the correct fixture for a given team
+            const teamFixtures = gameweekFixtures.map(
+              (fixture) => fixture[teamIndex]
+            );
+            return <FixturesRow team={team} teamFixtures={teamFixtures} />;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function FixturesRow({ team, teamFixtures }) {
+  return (
+    <tr>
+      <td>
+        <span>{team.name}</span>
+        <br />
+        <img src={team.badge} alt={team.name} className="fixtures-row-badge" />
+      </td>
+      {teamFixtures.map((fixture) => (
+        <FixtureCell
+          location={fixture.location}
+          opposition={fixture.initial}
+          team={fixture.initial}
+          badge={fixture.badge}
+          alt={fixture.name}
+        />
+      ))}
+    </tr>
+  );
+}
+
+function FixtureCell({ location, opposition, badge, name }) {
+  return (
+    <td>
+      {location} {opposition}{" "}
+      <img src={badge} alt={name} className="fixture-card-badge" />
+    </td>
   );
 }
